718e302211afe229370be1a9fa8ba2c2f59c4411
Geetask <42314408+Geetask@users.noreply.github.com>
2018-08-15 18:55:43 +0530

    Initial commit

---
+++ src/test/java/com/test/NewImplementation/AppTest.java
@@ -0,0 +1,15 @@
+package com.test.NewImplementation;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    {
+	@Test
+    public void test()
+    {
+    	System.out.println("hello");
+    }
+}
