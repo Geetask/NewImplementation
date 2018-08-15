d4cfb12f938d4cc490851b8357f42f0d2432a22a
Geetask <42314408+Geetask@users.noreply.github.com>
2018-08-15 21:05:11 +0530

    Selenium test project
    Selenium test project

--- src/test/java/com/test/NewImplementation/AppTest.java
+++ src/test/java/com/test/NewImplementation/AppTest.java
@@ -1,5 +1,9 @@
 package com.test.NewImplementation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 /**
@@ -8,10 +12,31 @@ import org.testng.annotations.Test;
 public class AppTest 
     {
 	@Test
-    public void test()
+    public void test() throws InterruptedException
     {
-    	System.out.println("hello");
-    	System.out.println("test git desktop");
-    	System.out.println("wel come to jenkins");
+		System.setProperty("webdriver.gecko.driver","F:\\software\\GECODriver\\geckodriver.exe");
+		  WebDriver driver = new FirefoxDriver();
+		  String baseUrl = "http://demo.guru99.com/test/newtours/";
+	        String expectedTitle = "Welcome: Mercury Tours";
+	        String actualTitle = "";
+
+	        // launch Fire fox and direct it to the Base URL
+	        driver.get(baseUrl);
+
+	        // get the actual value of the title
+	        actualTitle = driver.getTitle();
+
+	        /*
+	         * compare the actual title of the page with the expected one and print
+	         * the result as "Passed" or "Failed"
+	         */
+	        if (actualTitle.contentEquals(expectedTitle)){
+	            System.out.println("Test Passed!");
+	        } else {
+	            System.out.println("Test Failed");
+	        }
+	       
+	        //close Fire fox
+	        driver.close();
     }
 }
